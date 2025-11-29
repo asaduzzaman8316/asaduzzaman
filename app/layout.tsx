c907396426970e7a7163f76469efa1c7800622f4
Asaduzzaman9202 <asaduzzaman8316@gmail.com>
2025-11-29 20:11:48 +0600

    add speedInsights of vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Nav from "@/components/Share/Navbar/Nav";
 import Footer from "@/components/Share/Footer/Footer";
 import ScrollToTop from "@/components/Share/ScrollToTop/ScrollToTop";
 import Script from "next/script";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const font = Inter({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${font.className} antialiased bg-[#0d0d1f]`}
       >
+        <SpeedInsights />
         <Nav />
         {children}
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
